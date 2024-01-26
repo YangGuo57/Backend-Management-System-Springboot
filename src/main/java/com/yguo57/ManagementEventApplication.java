9cb6a263dfe3a5837fd32d220964979d2a74ef26
Yang Guo <yguo57@my.bcit.ca>
2024-01-25 21:28:01 -0800

    setup environment

---
+++ src/main/java/com/yguo57/ManagementEventApplication.java
@@ -0,0 +1,14 @@
+package com.yguo57;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class ManagementEventApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementEventApplication.class, args);
+    }
+}
