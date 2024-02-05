101f73422328681db9e8c131a090ff86cd58fdb5
Yang Guo <yguo57@my.bcit.ca>
2024-02-04 17:46:18 -0800

    Add lombok dependency

--- src/main/java/com/yguo57/pojo/Category.java
+++ src/main/java/com/yguo57/pojo/Category.java
@@ -3,10 +3,10 @@ package com.yguo57.pojo;
 import java.time.LocalDateTime;
 
 public class Category {
-    private Integer id;//主键ID
-    private String categoryName;//分类名称
-    private String categoryAlias;//分类别名
-    private Integer createUser;//创建人ID
-    private LocalDateTime createTime;//创建时间
-    private LocalDateTime updateTime;//更新时间
+    private Integer id;
+    private String categoryName;
+    private String categoryAlias;
+    private Integer createUser;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
 }
--- src/main/java/com/yguo57/pojo/User.java
+++ src/main/java/com/yguo57/pojo/User.java
@@ -5,12 +5,12 @@ package com.yguo57.pojo;
 import java.time.LocalDateTime;
 
 public class User {
-    private Integer id;//主键ID
-    private String username;//用户名
-    private String password;//密码
-    private String nickname;//昵称
-    private String email;//邮箱
-    private String userPic;//用户头像地址
-    private LocalDateTime createTime;//创建时间
-    private LocalDateTime updateTime;//更新时间
+    private Integer id;
+    private String username;
+    private String password;
+    private String nickname;
+    private String email;
+    private String userPic;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
 }
