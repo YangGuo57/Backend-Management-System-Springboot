dc040f3c1dfade5a221f9533fcc7c73951760c62
Yang Guo <yguo57@my.bcit.ca>
2024-02-08 14:55:42 -0800

    Complete register interface

--- src/main/java/com/yguo57/controller/UserController.java
+++ src/main/java/com/yguo57/controller/UserController.java
@@ -3,30 +3,30 @@ package com.yguo57.controller;
 import com.yguo57.pojo.Result;
 import com.yguo57.pojo.User;
 import com.yguo57.service.UserService;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
-    
+
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
-    public Result register(String username, String password) {
-        // search user
+    public Result register(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password) {
         User u = userService.findByUserName(username);
         if (u == null) {
             // if not exist, continue register
             userService.register(username, password);
             return Result.success();
         } else {
-            // if exist
-            return Result.error("You have already registered!");
+            return Result.error("Please enter valid username and pwd");
         }
     }
-
 }
