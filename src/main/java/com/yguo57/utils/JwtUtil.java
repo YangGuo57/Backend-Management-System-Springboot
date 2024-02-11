edd49df0d2ed63f8146b279feec0f345a459efef
Yang Guo <yguo57@my.bcit.ca>
2024-02-11 11:50:37 -0800

    Complete JWT

---
+++ src/main/java/com/yguo57/utils/JwtUtil.java
@@ -0,0 +1,29 @@
+package com.yguo57.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import java.util.Date;
+import java.util.Map;
+
+public class JwtUtil {
+
+    private static final String KEY = "yguo57";
+
+    // generate token
+    public static String genToken(Map<String, Object> claims) {
+        return JWT.create()
+                .withClaim("claims", claims)
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 12))
+                .sign(Algorithm.HMAC256(KEY));
+    }
+
+    // verify token
+    public static Map<String, Object> parseToken(String token) {
+        return JWT.require(Algorithm.HMAC256(KEY))
+                .build()
+                .verify(token)
+                .getClaim("claims")
+                .asMap();
+    }
+}
