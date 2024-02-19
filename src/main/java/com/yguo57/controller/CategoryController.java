b35c51259eee8b5d137a0b79c310f4c711db3134
Yang Guo <yguo57@my.bcit.ca>
2024-02-19 00:39:55 -0800

    Group Validation

--- src/main/java/com/yguo57/controller/CategoryController.java
+++ src/main/java/com/yguo57/controller/CategoryController.java
@@ -16,7 +16,7 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @PostMapping
-    public Result add(@RequestBody @Validated Category category) {
+    public Result add(@RequestBody @Validated(Category.Add.class) Category category) {
         categoryService.add(category);
         return Result.success();
     }
@@ -34,7 +34,7 @@ public class CategoryController {
     }
 
     @PutMapping
-    public Result update(@RequestBody @Validated Category category) {
+    public Result update(@RequestBody @Validated(Category.Update.class) Category category) {
         categoryService.update(category);
         return Result.success();
     }
--- src/main/java/com/yguo57/pojo/Category.java
+++ src/main/java/com/yguo57/pojo/Category.java
@@ -6,17 +6,29 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+
 @Data
 public class Category {
-    @NotNull
+    @NotNull(groups = Update.class)
     private Integer id;
-    @NotEmpty
+    @NotEmpty(groups = {Add.class, Update.class})
     private String categoryName;
-    @NotEmpty
+    @NotEmpty(groups = {Add.class, Update.class})
     private String categoryAlias;
     private Integer createUser;
     @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
     private LocalDateTime createTime;
     @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
     private LocalDateTime updateTime;
+
+    // Group Validation
+    public interface Add {
+
+    }
+    // Group Validation
+    public interface Update {
+
+    }
+
+
 }
